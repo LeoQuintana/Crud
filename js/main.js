de38afba291336b8b9018dd5b882d6c0ba69f30f
LeoQuintana <leoquintanaa@gmail.com>
2018-01-10 17:02:45 +0100

    ultimo avance

--- js/main.js
+++ js/main.js
@@ -9,16 +9,19 @@ $(document).ready(function () {
 		e.preventDefault();
 		user.usuario = $('#user').val();
 		user.correo = $('#correo').val();
-	var us = JSON. parse(JSON.stringify(user));
+	var us = JSON.parse(JSON.stringify(user));
 	users.push(us);
 	$('tbody').empty();
-	users.forEach(function (users) {
+	users.forEach(function (user) {
 		var tr = '<tr>' +
 		'<td>' + user.usuario + '</td>' + 
 		'<td>' + user.correo + '</td>' + 
-		'<td><button>eliminar</button></td>' + 
+		'<td><input type="button" id="btn" value="eliminar"></input></td>' + 
 		'</tr>'
 	$('tbody').append(tr);
 	});
+	$('table').on('click', 'input[type=button]', function (e) {
+		$(this).closest('tr').remove();
+	})
   });
 });
